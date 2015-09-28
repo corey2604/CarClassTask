61abc10b121cca15bf58d2653f381bf1ae7136e0
Kainos User <c.duffy@kainos>
2015-09-28 16:25:32 +0100

    Print Tests for class updates

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,17 @@ public class Main {
     public static void main(String[] args) {
         Engine v = new Engine("Honda","Petrol", 12.5f, 14.5f, 15.5f);
         Vehicle w = new Vehicle("Car", "Ford", "Fiesta", v);
-        //System.out.println(v.VehicleName + " " + v.Engine);
-        //v.intoEngine("Honda", "Petrol", "14.50", "12.50", "11.50");
-        //System.out.println(v.VehicleName+" "+v.Engine);
-        System.out.println(w.GetDescription());
-        System.out.println(v.GetEngine());
+        Engine x = new Engine("Honda","Petrol", 12.5f, 14.5f, 15.5f);
+        Vehicle y = new Vehicle("Car", "Nissan", "Micra", x);
+        //Testing that methods return correct values
+        System.out.println("Vehicle w is "+w.GetDescription());
+        System.out.println("Vehicle y is "+y.GetDescription());
+        System.out.println("Engine v is "+v.GetEngine());
+        System.out.println("Engine x is "+x.GetEngine());
+        System.out.println();
+        //Testing that engine x is updated but v remains the same
+        x.UpdateEngine("Nissan","Diesel", 24.5f, 18.5f, 20.5f);
+        System.out.println("Engine v is "+v.GetEngine());
+        System.out.println("Engine x is "+x.GetEngine());
     }
 }
